04506203e23b144df92e1050a1e9d472b881f085
unknown <troymnlsn@gmail.com>
2022-07-08 12:32:41 -0500

    testing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 
 var today = moment().format('MMMM Do YYYY, hh:mm:ss a');
 console.log(today);
-var 
+var sym = 'AAPL';
+
+fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${sym}&apikey=U9H8L320ZL3GRGKS`)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        let results = data;
+        console.log(results);
+        console.log(data.Description);
+    })  
+    .catch(function (err) {
+        console.error(err);
+    });
