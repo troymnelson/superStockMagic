ba9cfc681515869c366e925b713d259ae41ff8e4
unknown <troymnlsn@gmail.com>
2022-07-08 14:10:35 -0500

    added navbar and script tags

--- js/app.js
+++ js/app.js
@@ -3,15 +3,35 @@ var today = moment().format('MMMM Do YYYY, hh:mm:ss a');
 console.log(today);
 var sym = 'AAPL';
 
+
+var newPElText = today;
+var pEl = $('#time');
+
+$(pEl).text(today);
+
 fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${sym}&apikey=U9H8L320ZL3GRGKS`)
     .then(function (res) {
         return res.json();
     })
     .then(function (data) {
         let results = data;
+        // console.log(results);
         console.log(results);
-        console.log(data.Description);
     })  
     .catch(function (err) {
         console.error(err);
     });
+
+fetch(`https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&topics=technology&apikey=U9H8L320ZL3GRGKS`)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        let results = data;
+        // console.log(results);
+        console.log(results);
+    })  
+    .catch(function (err) {
+        console.error(err);
+    });
+
