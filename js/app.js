f0c5dafecdf5864b4544bb719439418164ea912f
fixedOtter <fo1152rc@go.minnstate.edu>
2022-07-08 22:47:19 -0500

    parsed some iex api data; cleaned up the fetch

--- js/app.js
+++ js/app.js
@@ -15,10 +15,47 @@ $('.p-aside').text="hello";
 $(pEl).text(today);
 
 // this is the onclick function for the stock search button
-$(`#stockSearchButton`).click(function() {
-  console.log($(`.materialize-textarea`).val());
-});
+$(`#stockSearchButton`).click(stockSearch);
 
+function stockSearch() {
+  let $userInput = $(`.materialize-textarea`).val();
+
+  // TODO add a validation to make sure the user is only typing in the ticker
+
+  /* grabbing stuff from iexcloud API */
+  // first requesting the data referencing the user input and our api token
+  fetch(`https://cloud.iexapis.com/stable/stock/${$userInput}/quote?token=${pub}`)
+  // processing the handshake 
+  .then(function (response) {
+    return response.json();
+  })
+  // hands shaken, data taken
+  .then(function (data) {
+    let data0 = data;
+    console.log(data0);
+  })
+  // catchall error bucket (thanks micheal!)
+  .catch(function(err) {
+    console.error(err);
+  });
+
+  let parsedData = {
+    companyName: data0.companyName,
+    pointChange: data0.change,
+    percentChange: data0.changePercent,
+    closePrice: data0.close,
+    closeTime: moment.unix(date0.closeTime).format("dddd, Do MMM YYYY, h:mm:ss A"),
+    afterHoursPointChange: data0.extendedChange,
+    afterHoursPercentChange: data0.extendedChangePercent,
+    afterHoursPrice: data0.extendedPrice,
+    afterHoursTime: moment.unix(date0.extendedPriceTime).format("dddd, Do MMM YYYY, h:mm:ss A"),
+    dayHigh: data0.high,
+    dayLow: data0.low,
+    isMarketOpen: data0.isUSMarketOpen,
+    lastUpdated: data0.iexLastUpdated,
+
+  }
+}
 
 /* grabbing the stock overview for a certain ticker */
 fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${userInputSym}&apikey=U9H8L320ZL3GRGKS`)
@@ -53,17 +90,6 @@ fetch(`https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&to
 
 
 
-/* grabbing stuff from iexcloud API */
-fetch(`https://cloud.iexapis.com/stable/stock/${userInputSym}/quote?token=${pub}`)
-  .then(function (response) {
-    let data0 = response;
-    console.log(data0);
-    return data0.json();
-})
-  .then(function (data) {
-    let results = data;
-    console.log(results);
-});
 
 
 
